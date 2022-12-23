d1876c45e992d66e3b19448c275df0f85ef369e2
Kunjesh Ramani <kunjeshramani9867@gmail.com>
2022-12-22 20:21:33 -0500

    Extra spaces are removed

--- Concept_Of_Class/First.java
+++ Concept_Of_Class/First.java
@@ -1,20 +1,14 @@
 package Concept_Of_Class;
 // type First.class = not human readable code. Run this in the command prompt
 class Second {
-
     public static void main(String[] args) {
         System.out.println("Second: Hello World");
         System.out.println("Second: Hello World");
         System.out.println("Second: Hello World");
     }
-    
 }
 class First {
-
     public static void main(String[] args) {
-        System.out.println("First: Hello World");
-        
-        
-    }
-    
+        System.out.println("First: Hello World");   
+    }   
 }
