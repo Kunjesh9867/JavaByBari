b58a661b55fbaa941c4126fdde46af46cfc4d474
Kunjesh Ramani <kunjeshramani9867@gmail.com>
2022-12-22 20:38:11 -0500

    Lines added

--- ReadKeyboard.java
+++ ReadKeyboard.java
@@ -3,5 +3,8 @@ import java.util.*;
 class ReadKeyboard{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your interger value here: ");
+        int x = sc.nextInt();
+        System.out.println(x);
     }
 }
